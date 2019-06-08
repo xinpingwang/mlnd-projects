8ec318f4f5dcbfe088d02b6f7f2cde43ce41adc9
XinPing Wang <xinping.wang@hotmail.com>
2019-06-08 15:54:51 +0800

    first submission for p5

--- p5-reinforcement-learning/Robot.py
+++ p5-reinforcement-learning/Robot.py
@@ -42,10 +42,10 @@ class Robot(object):
         """
         if self.testing:
             # TODO 1. No random choice when testing
-            pass
+            self.epsilon = 0
         else:
             # TODO 2. Update parameters when learning
-            pass
+            self.epsilon *= 0.9
 
         return self.epsilon
 
@@ -55,7 +55,7 @@ class Robot(object):
         """
 
         # TODO 3. Return robot's current state
-        return None
+        return self.maze.sense_robot()
 
     def create_Qtable_line(self, state):
         """
@@ -66,7 +66,7 @@ class Robot(object):
         # Qtable[state] ={'u':xx, 'd':xx, ...}
         # If Qtable[state] already exits, then do
         # not change it.
-        pass
+        self.Qtable.setdefault(state, {x: 0.0 for x in self.valid_actions})
 
     def choose_action(self):
         """
@@ -77,28 +77,43 @@ class Robot(object):
             # TODO 5. Return whether do random choice
             # hint: generate a random number, and compare
             # it with epsilon
-            pass
+            return random.random() < self.epsilon
 
         if self.learning:
             if is_random_exploration():
                 # TODO 6. Return random choose aciton
-                return None
+                return random.choice(self.valid_actions)
             else:
                 # TODO 7. Return action with highest q value
-                return None
+                action = None
+                for k, v in self.Qtable[self.state].items():
+                    if action == None:
+                        action = k
+                    elif self.Qtable[self.state][action] < v:
+                        action = k
+                return action
         elif self.testing:
             # TODO 7. choose action with highest q value
+            action = None
+            for k, v in self.Qtable[self.state].items():
+                if action == None:
+                    action = k
+                elif self.Qtable[self.state][action] < v:
+                    action = k
+            return action
         else:
             # TODO 6. Return random choose aciton
+            return random.choice(self.valid_actions)
 
     def update_Qtable(self, r, action, next_state):
         """
         Update the qtable according to the given rule.
         """
         if self.learning:
-            pass
             # TODO 8. When learning, update the q table according
             # to the given rules
+            max_nq = max(self.Qtable[next_state].values())
+            self.Qtable[self.state][action] += self.alpha * (r + self.gamma * max_nq - self.Qtable[self.state][action])
 
     def update(self):
         """
@@ -110,6 +125,7 @@ class Robot(object):
         self.create_Qtable_line(self.state) # For the state, create q table line
 
         action = self.choose_action() # choose action for this state
+        
         reward = self.maze.move_robot(action) # move robot for given action
 
         next_state = self.sense_state() # get next state
